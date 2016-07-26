23a3ebe088a5b64a3cebf47feb660f236934f867
Sam Whited <sam@samwhited.com>
2016-07-25 19:12:02 -0500

    Fix name of SCRAM mechanisms

--- scram.go
+++ scram.go
@@ -224,12 +224,12 @@ func ScramSha256Plus(identity, username, password string) Mechanism {
 // mechanism defined in RFC 5802. Each call to the function returns a new
 // Mechanism with its own internal state.
 func ScramSha1(identity, username, password string) Mechanism {
-	return scram(identity, username, password, "SCRAM-SHA-1-PLUS", nonce(noncerandlen, cryptoReader{}), sha1.New)
+	return scram(identity, username, password, "SCRAM-SHA-1", nonce(noncerandlen, cryptoReader{}), sha1.New)
 }
 
 // ScramSha256 returns a Mechanism that implements the SCRAM-SHA-256
 // authentication mechanism defined in RFC 7677. Each call to the function
 // returns a new Mechanism with its own internal state.
 func ScramSha256(identity, username, password string) Mechanism {
-	return scram(identity, username, password, "SCRAM-SHA-256-PLUS", nonce(noncerandlen, cryptoReader{}), sha256.New)
+	return scram(identity, username, password, "SCRAM-SHA-256", nonce(noncerandlen, cryptoReader{}), sha256.New)
 }
