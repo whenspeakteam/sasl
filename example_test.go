d82250070423d6430e5183c2721cfc981b8bce85
Sam Whited <sam@samwhited.com>
2016-08-03 19:31:18 -0500

    Add example custom mechanism

---
+++ example_test.go
@@ -0,0 +1,56 @@
+// Copyright 2016 Sam Whited.
+// Use of this source code is governed by the BSD 2-clause license that can be
+// found in the LICENSE file.
+
+package sasl_test
+
+import (
+	"fmt"
+
+	"mellium.im/sasl"
+)
+
+// A custom SASL Mechanism that implements XOAUTH2:
+// https://developers.google.com/gmail/xoauth2_protocol
+var xoauth2 = sasl.Mechanism{
+	Name: "XOAUTH2",
+	Start: func(m sasl.Negotiator) (bool, []byte, error) {
+		c := m.Config()
+
+		payload := []byte(`user=`)
+		payload = append(payload, c.Username...)
+		payload = append(payload, '\x01')
+		payload = append(payload, []byte(`auth=Bearer `)...)
+		payload = append(payload, c.Password...)
+		payload = append(payload, '\x01', '\x01')
+
+		// We do not need to Base64 encode the payload; the sasl.Negotiator will do
+		// that for us.
+		return false, payload, nil
+	},
+	Next: func(m sasl.Negotiator, challenge []byte) (bool, []byte, error) {
+		// If we're a client or a server that's past the AuthTextSent step, we
+		// should never actually hit this step for the XOAUTH2 mechanism so return
+		// an error.
+		if m.State()&sasl.Receiving != sasl.Receiving || m.State()&sasl.StepMask != sasl.AuthTextSent {
+			return false, nil, sasl.ErrTooManySteps
+		}
+
+		// The server will take the auth from here. We don't really do much with
+		// this mechanism since there is only one step.
+		return false, challenge, nil
+	},
+}
+
+func Example_customMechanism() {
+	c := sasl.NewClient(xoauth2, sasl.Credentials(
+		"someuser@example.com", "vF9dft4qmTc2Nvb3RlckBhdHRhdmlzdGEuY29tCg=="),
+	)
+
+	// This is the first step and we haven't received any challenge from the
+	// server yet.
+	more, resp, _ := c.Step(nil)
+	fmt.Printf("%v %s", more, resp)
+
+	// Output: false dXNlcj1zb21ldXNlckBleGFtcGxlLmNvbQFhdXRoPUJlYXJlciB2RjlkZnQ0cW1UYzJOdmIzUmxja0JoZEhSaGRtbHpkR0V1WTI5dENnPT0BAQ==
+}
