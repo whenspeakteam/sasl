eb02513c2363ad37952720a3715f46a7c970c4cc
Sam Whited <sam@samwhited.com>
2017-07-09 08:58:53 -0500

    Remove Go 1.6 support from tests

--- sasl_test.go
+++ sasl_test.go
@@ -7,6 +7,7 @@ package sasl
 import (
 	"crypto/sha1"
 	"encoding/base64"
+	"fmt"
 	"testing"
 )
 
@@ -110,6 +111,17 @@ func BenchmarkPlain(b *testing.B) {
 			if !more {
 				break
 			}
+		}
+	}
+}
+
+func doTests(t *testing.T, cases []testCases, fn func(t *testing.T, tc saslTest)) {
+	for _, g := range cases {
+		for i, tc := range g.cases {
+			name := fmt.Sprintf("%s:%d", g.name, i)
+			t.Run(name, func(t *testing.T) {
+				fn(t, tc)
+			})
 		}
 	}
 }
