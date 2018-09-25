cd896e5b6deddcd8d0e29ea51ae5ca3b1d06ffe7
Sam Whited <sam@samwhited.com>
2018-09-24 22:44:44 -0500

    sasl: factor out client side of scram
    Prepare to implement the server side of scram by extracting the client
    side into a new function so that the Next method isn't cluttered up with
    logic from both.

--- scram.go
+++ scram.go
@@ -88,138 +88,143 @@ func scram(name string, fn func() hash.Hash) Mechanism {
 			return true, append(getGS2Header(name, m), clientFirstMessage...), clientFirstMessage, nil
 		},
 		Next: func(m *Negotiator, challenge []byte, data interface{}) (more bool, resp []byte, cache interface{}, err error) {
-			_, password, _ := m.Credentials()
-			state := m.State()
 			if challenge == nil || len(challenge) == 0 {
-				err = ErrInvalidChallenge
-				return
+				return more, resp, cache, ErrInvalidChallenge
 			}
+			state := m.State()
 
 			// BUG(ssw): The server side of SCRAM is not yet implemented.
 			if state&Receiving == Receiving {
 				panic("sasl: Server side of SCRAM not yet implemented")
 			}
 
-			switch state & StepMask {
-			case AuthTextSent:
-				iter := -1
-				var salt, nonce []byte
-				for _, field := range bytes.Split(challenge, []byte{','}) {
-					if len(field) < 3 || (len(field) >= 2 && field[1] != '=') {
-						continue
-					}
-					switch field[0] {
-					case 'i':
-						ival := string(bytes.TrimRight(field[2:], "\x00"))
-
-						if iter, err = strconv.Atoi(ival); err != nil {
-							return
-						}
-					case 's':
-						salt = make([]byte, base64.StdEncoding.DecodedLen(len(field)-2))
-						var n int
-						n, err = base64.StdEncoding.Decode(salt, field[2:])
-						salt = salt[:n]
-						if err != nil {
-							return
-						}
-					case 'r':
-						nonce = field[2:]
-					case 'm':
-						// RFC 5802:
-						// m: This attribute is reserved for future extensibility.  In this
-						// version of SCRAM, its presence in a client or a server message
-						// MUST cause authentication failure when the attribute is parsed by
-						// the other end.
-						err = errors.New("Server sent reserved attribute `m'")
-						return
-					}
-				}
-
-				switch {
-				case iter < 0:
-					err = errors.New("Iteration count is missing")
-					return
-				case iter < 0:
-					err = errors.New("Iteration count is invalid")
-					return
-				case nonce == nil || !bytes.HasPrefix(nonce, m.Nonce()):
-					err = errors.New("Server nonce does not match client nonce")
-					return
-				case salt == nil:
-					err = errors.New("Server sent empty salt")
-					return
-				}
-
-				gs2Header := getGS2Header(name, m)
-				tlsState := m.TLSState()
-				var channelBinding []byte
-				if tlsState != nil && strings.HasSuffix(name, "-PLUS") {
-					channelBinding = make(
-						[]byte,
-						2+base64.StdEncoding.EncodedLen(len(gs2Header)+len(tlsState.TLSUnique)),
-					)
-					base64.StdEncoding.Encode(channelBinding[2:], append(gs2Header, tlsState.TLSUnique...))
-					channelBinding[0] = 'c'
-					channelBinding[1] = '='
-				} else {
-					channelBinding = make(
-						[]byte,
-						2+base64.StdEncoding.EncodedLen(len(gs2Header)),
-					)
-					base64.StdEncoding.Encode(channelBinding[2:], gs2Header)
-					channelBinding[0] = 'c'
-					channelBinding[1] = '='
-				}
-				clientFinalMessageWithoutProof := append(channelBinding, []byte(",r=")...)
-				clientFinalMessageWithoutProof = append(clientFinalMessageWithoutProof, nonce...)
-
-				clientFirstMessage := data.([]byte)
-				authMessage := append(clientFirstMessage, ',')
-				authMessage = append(authMessage, challenge...)
-				authMessage = append(authMessage, ',')
-				authMessage = append(authMessage, clientFinalMessageWithoutProof...)
-
-				saltedPassword := pbkdf2.Key(password, salt, iter, fn().Size(), fn)
-
-				h := hmac.New(fn, saltedPassword)
-				h.Write(serverKeyInput)
-				serverKey := h.Sum(nil)
-				h.Reset()
-
-				h.Write(clientKeyInput)
-				clientKey := h.Sum(nil)
-
-				h = hmac.New(fn, serverKey)
-				h.Write(authMessage)
-				serverSignature := h.Sum(nil)
-
-				h = fn()
-				h.Write(clientKey)
-				storedKey := h.Sum(nil)
-				h = hmac.New(fn, storedKey)
-				h.Write(authMessage)
-				clientSignature := h.Sum(nil)
-				clientProof := make([]byte, len(clientKey))
-				xorBytes(clientProof, clientKey, clientSignature)
-
-				encodedClientProof := make([]byte, base64.StdEncoding.EncodedLen(len(clientProof)))
-				base64.StdEncoding.Encode(encodedClientProof, clientProof)
-				clientFinalMessage := append(clientFinalMessageWithoutProof, []byte(",p=")...)
-				clientFinalMessage = append(clientFinalMessage, encodedClientProof...)
-
-				return true, clientFinalMessage, serverSignature, nil
-			case ResponseSent:
-				clientCalculatedServerFinalMessage := "v=" + base64.StdEncoding.EncodeToString(data.([]byte))
-				if clientCalculatedServerFinalMessage != string(challenge) {
-					err = ErrAuthn
-					return
-				}
-				// Success!
-				return false, nil, nil, nil
-			}
-			err = ErrInvalidState
-			return
+			return scramClientNext(name, fn, m, challenge, data)
 		},
 	}
+}
+
+func scramClientNext(name string, fn func() hash.Hash, m *Negotiator, challenge []byte, data interface{}) (more bool, resp []byte, cache interface{}, err error) {
+	_, password, _ := m.Credentials()
+	state := m.State()
+
+	switch state & StepMask {
+	case AuthTextSent:
+		iter := -1
+		var salt, nonce []byte
+		for _, field := range bytes.Split(challenge, []byte{','}) {
+			if len(field) < 3 || (len(field) >= 2 && field[1] != '=') {
+				continue
+			}
+			switch field[0] {
+			case 'i':
+				ival := string(bytes.TrimRight(field[2:], "\x00"))
+
+				if iter, err = strconv.Atoi(ival); err != nil {
+					return
+				}
+			case 's':
+				salt = make([]byte, base64.StdEncoding.DecodedLen(len(field)-2))
+				var n int
+				n, err = base64.StdEncoding.Decode(salt, field[2:])
+				salt = salt[:n]
+				if err != nil {
+					return
+				}
+			case 'r':
+				nonce = field[2:]
+			case 'm':
+				// RFC 5802:
+				// m: This attribute is reserved for future extensibility.  In this
+				// version of SCRAM, its presence in a client or a server message
+				// MUST cause authentication failure when the attribute is parsed by
+				// the other end.
+				err = errors.New("Server sent reserved attribute `m'")
+				return
+			}
+		}
+
+		switch {
+		case iter < 0:
+			err = errors.New("Iteration count is missing")
+			return
+		case iter < 0:
+			err = errors.New("Iteration count is invalid")
+			return
+		case nonce == nil || !bytes.HasPrefix(nonce, m.Nonce()):
+			err = errors.New("Server nonce does not match client nonce")
+			return
+		case salt == nil:
+			err = errors.New("Server sent empty salt")
+			return
+		}
+
+		gs2Header := getGS2Header(name, m)
+		tlsState := m.TLSState()
+		var channelBinding []byte
+		if tlsState != nil && strings.HasSuffix(name, "-PLUS") {
+			channelBinding = make(
+				[]byte,
+				2+base64.StdEncoding.EncodedLen(len(gs2Header)+len(tlsState.TLSUnique)),
+			)
+			base64.StdEncoding.Encode(channelBinding[2:], append(gs2Header, tlsState.TLSUnique...))
+			channelBinding[0] = 'c'
+			channelBinding[1] = '='
+		} else {
+			channelBinding = make(
+				[]byte,
+				2+base64.StdEncoding.EncodedLen(len(gs2Header)),
+			)
+			base64.StdEncoding.Encode(channelBinding[2:], gs2Header)
+			channelBinding[0] = 'c'
+			channelBinding[1] = '='
+		}
+		clientFinalMessageWithoutProof := append(channelBinding, []byte(",r=")...)
+		clientFinalMessageWithoutProof = append(clientFinalMessageWithoutProof, nonce...)
+
+		clientFirstMessage := data.([]byte)
+		authMessage := append(clientFirstMessage, ',')
+		authMessage = append(authMessage, challenge...)
+		authMessage = append(authMessage, ',')
+		authMessage = append(authMessage, clientFinalMessageWithoutProof...)
+
+		saltedPassword := pbkdf2.Key(password, salt, iter, fn().Size(), fn)
+
+		h := hmac.New(fn, saltedPassword)
+		h.Write(serverKeyInput)
+		serverKey := h.Sum(nil)
+		h.Reset()
+
+		h.Write(clientKeyInput)
+		clientKey := h.Sum(nil)
+
+		h = hmac.New(fn, serverKey)
+		h.Write(authMessage)
+		serverSignature := h.Sum(nil)
+
+		h = fn()
+		h.Write(clientKey)
+		storedKey := h.Sum(nil)
+		h = hmac.New(fn, storedKey)
+		h.Write(authMessage)
+		clientSignature := h.Sum(nil)
+		clientProof := make([]byte, len(clientKey))
+		xorBytes(clientProof, clientKey, clientSignature)
+
+		encodedClientProof := make([]byte, base64.StdEncoding.EncodedLen(len(clientProof)))
+		base64.StdEncoding.Encode(encodedClientProof, clientProof)
+		clientFinalMessage := append(clientFinalMessageWithoutProof, []byte(",p=")...)
+		clientFinalMessage = append(clientFinalMessage, encodedClientProof...)
+
+		return true, clientFinalMessage, serverSignature, nil
+	case ResponseSent:
+		clientCalculatedServerFinalMessage := "v=" + base64.StdEncoding.EncodeToString(data.([]byte))
+		if clientCalculatedServerFinalMessage != string(challenge) {
+			err = ErrAuthn
+			return
+		}
+		// Success!
+		return false, nil, nil, nil
+	}
+	err = ErrInvalidState
+	return
 }
